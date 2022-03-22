91e61d8ffc5ecb124b3a3df553097fd59da3a18d
Tenebrosful <hugobernard39@gmail.com>
2022-03-22 15:34:40 +0100

    Revert "Add route :id/self-event"
    This reverts commit 511c003ee4462c41ad2562d703534b2783ea8db5.

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { User } from "../../database/models/User";
 import error404 from "../errors/error404";
 import error405 from "../errors/error405";
+import { ievent } from "../responseInterface/eventResponse";
 import { iallSelfEvents, iallUsers, iuser } from "../responseInterface/userResponse";
 
 const userRouter = express.Router();
@@ -54,41 +55,6 @@ userRouter.get("/:id", async (req, res, next) => {
     };
 
     res.status(200).json(result);
-  } catch (e) { next(e); }
-});
-
-userRouter.get("/:id/self-event", async (req, res, next) => {
-  try {
-    const user = await User.findOne(
-      {
-        attributes: [ "id" ],
-        where: {
-          id: req.params.id
-        }
-      });
-
-    if (!user) { error404(req, res, `L'utilisateur '${req.params.id}' est introuvable. (Potentiellement soft-delete)`); return; }
-
-    const selfEvents = await user.$get("ownedEvents");
-
-    const result: iallSelfEvents = {
-      count: selfEvents.length,
-      events: []
-    };
-
-    selfEvents.forEach(selfEvent => result.events.push({
-      coords: {
-        address: selfEvent.address,
-        lat: selfEvent.lat,
-        long: selfEvent.long,
-      },
-      description: selfEvent.description,
-      id: selfEvent.id,
-      title: selfEvent.title,
-    }));
-
-    res.status(200).json(result);
-
   } catch (e) { next(e); }
 });
 
