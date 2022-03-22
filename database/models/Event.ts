8a8b8504ef1b49aefba383d46e45b5f2c1ad33f3
Tenebrosful <hugobernard39@gmail.com>
2022-03-22 10:30:48 +0100

    Create Event.ts

---
+++ database/models/Event.ts
@@ -0,0 +1,34 @@
+import { BelongsTo, Column, DataType, Default, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { User } from "./User";
+
+@Table({paranoid: true, tableName: "event", timestamps: true})
+export class Event extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUIDV4)
+  id: string;
+
+  @Column(DataType.STRING(128))
+  title: string;
+
+  @Default(null)
+  @Column(DataType.TEXT)
+  description: string;
+
+  @Column(DataType.STRING(256))
+  address: string;
+
+  @Column(DataType.DOUBLE)
+  lat: number;
+
+  @Column(DataType.DOUBLE)
+  long: number;
+
+  @Default(null)
+  @ForeignKey(() => User)
+  @Column(DataType.UUIDV4)
+  owner_id: string;
+
+  @BelongsTo(() => User, "owner_id")
+  owner: User;
+}
