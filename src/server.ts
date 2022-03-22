813a8abbd105d40e47ec910f72bae113ea3af125
Tenebrosful <hugobernard39@gmail.com>
2022-03-22 13:35:26 +0100

    Initialize database and add userRouter

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import * as express from "express";
+import { initBDD } from "../database/database";
 
 import * as bodyParser from "body-parser";
 import logger from "./middleware/logger";
+
+import userRouter from "./routes/user";
 
 import error400 from "./errors/error400";
 import error500 from "./errors/error500";
@@ -9,10 +12,17 @@ import error500 from "./errors/error500";
 const app = express();
 const port = process.env.EXPRESS_PORT || 3000;
 
+initBDD();
+
 app.use(logger);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+/**
+ * Routes
+ */
+app.use("/api/user", userRouter);
 
 /**
  * Handle Errors
@@ -22,6 +32,5 @@ app.use(error400);
 app.use(error500); // Must be on last position
 
 app.listen(port, () => {
-    console.log("PORT " + process.env.EXPRESS_PORT);
     console.log(`Server started at port http://localhost:${port}`);
 });
