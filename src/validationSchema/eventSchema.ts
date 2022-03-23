d48530ac1640e5e811ab2084f977dec95baef833
Tenebrosful <hugobernard39@gmail.com>
2022-03-23 15:15:52 +0100

    Create eventSchema.ts

---
+++ src/validationSchema/eventSchema.ts
@@ -0,0 +1,15 @@
+import * as Joi from "joi";
+
+const eventSchema = Joi.object({
+  coords: {
+    address: Joi.string(),
+    lat: Joi.number().min(-90).max(90),
+    long: Joi.number().min(-90).max(90),
+  },
+  date: Joi.date().greater("now").optional(),
+  description: Joi.string().optional(),
+  owner_id: Joi.string().uuid().optional(),
+  title: Joi.string().alphanum(),
+});
+
+export default eventSchema;
