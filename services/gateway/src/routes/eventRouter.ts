4a9e17bea1d7bba61a30da8261b6a91ebe7282f8
Geoffrey <61802648+GeoffreyPorayko@users.noreply.github.com>
2022-03-28 13:01:51 +0200

    ajout de quelques routes

--- services/gateway/src/routes/eventRouter.ts
+++ services/gateway/src/routes/eventRouter.ts
@@ -21,4 +21,61 @@ eventRouter.post('/', authRequired(), async (req, res, next) => {
     }
 });
 
+eventRouter.get("/:id", async (req, res, next) => {
+    
+    try {
+        const response = await axios.get(process.env.API_MAIN_URL + '/event/' + req.params.id);
+
+        res.status(response.status).json(response.data);
+    } catch (e) {
+        
+        // @ts-ignore
+        if(e.isAxiosError && e.response && e.response.status !== 500) {
+            // @ts-ignore
+            res.status(e.response.status).json(e.response.data); return;
+        }
+
+        next(e);
+    }
+});
+
+eventRouter.get("/:id/participants", async (req, res, next) => {
+    
+    try {
+        const response = await axios.get(process.env.API_MAIN_URL + '/event/' + req.params.id + '/participants');
+
+        res.status(response.status).json(response.data);
+    } catch (e) {
+        
+        // @ts-ignore
+        if(e.isAxiosError && e.response && e.response.status !== 500) {
+            // @ts-ignore
+            res.status(e.response.status).json(e.response.data); return;
+        }
+
+        next(e);
+    }
+});
+
+eventRouter.get("/:id/comments", async (req, res, next) => {
+    let option = '';
+
+    if (req.query.embedAuthor) option = '?embedAuthor=true';
+
+    try {
+        const response = await axios.get(process.env.API_MAIN_URL + '/event/' + req.params.id + '/comments' + option);
+
+        res.status(response.status).json(response.data);
+    } catch (e) {
+        
+        // @ts-ignore
+        if(e.isAxiosError && e.response && e.response.status !== 500) {
+            // @ts-ignore
+            res.status(e.response.status).json(e.response.data); return;
+        }
+
+        next(e);
+    }
+});
+
 export default eventRouter;
