c4e84a2fb01bc7cf66d1e939453be6c4e7be2813
Tenebrosful <hugobernard39@gmail.com>
2022-03-29 15:31:06 +0200

    Inverse route order

--- services/gateway/src/routes/authRouter.ts
+++ services/gateway/src/routes/authRouter.ts
@@ -1,8 +1,28 @@
 import * as express from "express";
 
 import axios from "axios";
+import error405 from "../errors/error405";
 
 const authRouter = express.Router();
+
+authRouter.post('/', async (req, res, next) => {
+    try {
+        const response = await axios.post(process.env.API_AUTH_URL + '/auth', req.body);
+        
+        res.status(response.status).json(response.data);
+    } catch (e) {
+        
+        // @ts-ignore
+        if(e.isAxiosError && e.response && e.response.status !== 500) {
+            // @ts-ignore
+            res.status(e.response.status).json(e.response.data); return;
+        }
+
+        next(e);
+    }
+});
+
+authRouter.all("/", error405(["POST"]));
 
 authRouter.post('/inscription', async (req, res, next) => {
     try {
@@ -21,21 +41,6 @@ authRouter.post('/inscription', async (req, res, next) => {
     }
 });
 
-authRouter.post('/', async (req, res, next) => {
-    try {
-        const response = await axios.post(process.env.API_AUTH_URL + '/auth', req.body);
-        
-        res.status(response.status).json(response.data);
-    } catch (e) {
-        
-        // @ts-ignore
-        if(e.isAxiosError && e.response && e.response.status !== 500) {
-            // @ts-ignore
-            res.status(e.response.status).json(e.response.data); return;
-        }
-
-        next(e);
-    }
-});
+authRouter.all("/inscription", error405(["POST"]));
 
 export default authRouter;
