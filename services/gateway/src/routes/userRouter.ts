fbd33244f89a9d86720b62bd7b8f4f91dfa37e5a
Tenebrosful <hugobernard39@gmail.com>
2022-03-29 12:12:24 +0200

    Add self-event route

--- services/gateway/src/routes/userRouter.ts
+++ services/gateway/src/routes/userRouter.ts
@@ -64,4 +64,27 @@ userRouter.get("/:id/joined-event", async (req, res, next) => {
 
 userRouter.all("/:id/joined-event", error405(["GET"]));
 
+userRouter.get("/:id/self-event", async (req, res, next) => {
+  const params = new url.URLSearchParams();
+
+  if (req.query.participants) params.append("participants", req.query.participants as string);
+  if (req.query.embedOwner) params.append("embedOwner", req.query.embedOwner as string);
+
+  try {
+    const response = await axios.get(`${process.env.API_MAIN_URL}/user/${req.params.id}/self-event`, { params });
+    res.status(response.status).json(response.data);
+  } catch (e) {
+
+    // @ts-ignore
+    if (e.isAxiosError && e.response && e.response.status !== 500) {
+      // @ts-ignore
+      res.status(e.response.status).json(e.response.data); return;
+    }
+
+    next(e);
+  }
+});
+
+userRouter.all("/:id/self-event", error405(["GET"]));
+
 export default userRouter;
